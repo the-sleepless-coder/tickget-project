f288031e3f94078c9b9f0157b7d5f4036c5fa321
0224LJH <kavae550@gmail.com>
2025-10-27 17:13:26 +0900

    style: api 경로 양식 변경 [S13P31A209-100]

--- Backend/room-server/src/main/java/com/tickget/roomserver/controller/RoomController.java
+++ Backend/room-server/src/main/java/com/tickget/roomserver/controller/RoomController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/rooms")
+@RequestMapping("rooms")
 @RequiredArgsConstructor
 public class RoomController {
 
