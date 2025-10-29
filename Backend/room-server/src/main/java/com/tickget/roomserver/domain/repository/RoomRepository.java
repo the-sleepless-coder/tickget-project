66c5d9804b2f803c8bd2ae2ef86b3995ad7915a7
0224LJH <kavae550@gmail.com>
2025-10-29 10:28:13 +0900

    feat: RoomRepository 구현 [S13P31A209-102]

---
+++ Backend/room-server/src/main/java/com/tickget/roomserver/domain/repository/RoomRepository.java
@@ -0,0 +1,9 @@
+package com.tickget.roomserver.domain.repository;
+
+import com.tickget.roomserver.domain.entity.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long> {
+}
