d7e2c0ffc757e92edd24d1883f9b07ce84fabb7a
0224LJH <kavae550@gmail.com>
2025-10-29 10:26:21 +0900

    feat: Room Entity에 of 메서드 추가 [S13P31A209-102]

--- Backend/room-server/src/main/java/com/tickget/roomserver/domain/entity/Room.java
+++ Backend/room-server/src/main/java/com/tickget/roomserver/domain/entity/Room.java
@@ -3,6 +3,7 @@ package com.tickget.roomserver.domain.entity;
 import com.tickget.roomserver.domain.enums.HallType;
 import com.tickget.roomserver.domain.enums.RoomStatus;
 import com.tickget.roomserver.domain.enums.RoomType;
+import com.tickget.roomserver.dto.request.CreateRoomRequest;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -49,6 +50,17 @@ public class Room extends BaseTimeEntity{
     @Column(name = "thumbnail_url", length = 500)
     private String thumbnailUrl;
 
+    public static Room of (CreateRoomRequest createRoomRequest) {
+        return Room.builder()
+                .roomType(createRoomRequest.getRoomType())
+                .hallId(createRoomRequest.getHallId())
+                .hallType(createRoomRequest.getHallType())
+                .botCount(createRoomRequest.getBotCount())
+                .totalSeat(createRoomRequest.getTotalSeat())
+                .status(RoomStatus.READY)
+                .thumbnailUrl("") //TODO: 썸네일 관련 로직은 얘기해보고 추가 구현
+                .build();
+    }
 
 
 }
