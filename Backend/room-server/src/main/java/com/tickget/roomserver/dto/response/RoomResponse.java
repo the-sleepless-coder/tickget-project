9986bbe3cca129f65e2a08868f2957e4949507d4
0224LJH <kavae550@gmail.com>
2025-10-29 14:21:49 +0900

    feat: RoomResponse 구현 [S13P31A209-102]

---
+++ Backend/room-server/src/main/java/com/tickget/roomserver/dto/response/RoomResponse.java
@@ -0,0 +1,50 @@
+package com.tickget.roomserver.dto.response;
+
+import com.tickget.roomserver.domain.entity.Room;
+import com.tickget.roomserver.domain.enums.HallSize;
+import com.tickget.roomserver.domain.enums.RoomStatus;
+import com.tickget.roomserver.domain.enums.RoomType;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RoomResponse {
+
+    private Long roomId;
+    private String roomName;
+    private int botCount;
+    private int maxUserCount;
+    private int currentUserCount;
+
+    private String difficulty;
+    private RoomType roomType;
+    private RoomStatus status;
+    private LocalDateTime startTime;
+
+    private HallSize hallSize;
+    private String hallName;
+
+    public static RoomResponse of (Room room, MatchResponse matchResponse, int currentUserCount ){
+        return RoomResponse.builder()
+                .roomId(room.getId())
+                .roomName(matchResponse.getMatchTitle())
+                .botCount(room.getBotCount())
+                .maxUserCount(matchResponse.getMaxUserCount())
+                .currentUserCount(currentUserCount)
+                .difficulty(matchResponse.getDifficulty())
+                .roomType(room.getRoomType())
+                .status(room.getStatus())
+                .startTime(matchResponse.getStartTime())
+                .hallSize(room.getHallSize())
+                .hallName(room.getHallName())
+                .build();
+    }
+
+
+}
