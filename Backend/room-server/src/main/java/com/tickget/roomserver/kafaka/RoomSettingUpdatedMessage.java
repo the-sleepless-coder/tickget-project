2696b23385c7ca54256c53c1984a7d09d9c51efe
0224LJH <kavae550@gmail.com>
2025-11-04 15:43:39 +0900

    feat: 설정 업데이트 관련 이벤트와 메시지 구현 [S13P31A209-121]

---
+++ Backend/room-server/src/main/java/com/tickget/roomserver/kafaka/RoomSettingUpdatedMessage.java
@@ -0,0 +1,38 @@
+package com.tickget.roomserver.kafaka;
+
+import com.tickget.roomserver.domain.enums.Difficulty;
+import com.tickget.roomserver.domain.enums.EventType;
+import com.tickget.roomserver.event.RoomSettingUpdatedEvent;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RoomSettingUpdatedMessage {
+    private EventType eventType = EventType.ROOM_SETTING_UPDATED;
+    private Long roomId;
+    private String message;
+
+    private String roomName;
+    private String difficulty;
+    private Integer maxUserCount;
+    private LocalDateTime startTime;
+
+    static public RoomSettingUpdatedMessage from (RoomSettingUpdatedEvent roomSettingUpdatedEvent) {
+        return RoomSettingUpdatedMessage.builder()
+                .roomId(roomSettingUpdatedEvent.getRoomId())
+                .message("방 설정이 변경되었습니다.")
+                .roomName(roomSettingUpdatedEvent.getMatchName())
+                .difficulty(roomSettingUpdatedEvent.getDifficulty())
+                .maxUserCount(roomSettingUpdatedEvent.getMaxUserCount())
+                .startTime(roomSettingUpdatedEvent.getStartTime())
+                .build();
+    }
+
+
+}
