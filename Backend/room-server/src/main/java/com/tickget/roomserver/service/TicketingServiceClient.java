46863e10cef17e25abbe2ce23ab9f077017734ad
0224LJH <kavae550@gmail.com>
2025-11-04 13:04:46 +0900

    feat: TicketingServiceClient 기초 구현 [S13P31A209-121]

---
+++ Backend/room-server/src/main/java/com/tickget/roomserver/service/TicketingServiceClient.java
@@ -0,0 +1,39 @@
+package com.tickget.roomserver.service;
+
+import com.tickget.roomserver.dto.request.CreateMatchRequest;
+import com.tickget.roomserver.dto.response.MatchResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class TicketingServiceClient {
+
+    private final RestTemplate restTemplate;
+
+    @Value("${ticketing-service.url}")
+    private String ticketingServiceUrl;
+
+    public MatchResponse createMatch(CreateMatchRequest request) {
+        log.info("방 {}에 매치 생성 요청", request.getRoomId());
+
+        try {
+            ResponseEntity<MatchResponse> response = restTemplate.postForEntity(
+                    ticketingServiceUrl + "/matches",
+                    request,
+                    MatchResponse.class
+            );
+            log.info("방 {}에 매치 {} 생성 성공", response.getBody().getId(), response.getBody().getId());;
+            return response.getBody();
+
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+}
