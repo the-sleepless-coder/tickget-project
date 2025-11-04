0b44d7070b49b9579c4cb6351189c0ae70b4e37b
0224LJH <kavae550@gmail.com>
2025-11-04 15:43:39 +0900

    feat: 설정 업데이트 관련 이벤트와 메시지 구현 [S13P31A209-121]

---
+++ Backend/room-server/src/main/java/com/tickget/roomserver/event/RoomSettingUpdatedEvent.java
@@ -0,0 +1,27 @@
+package com.tickget.roomserver.event;
+
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class RoomSettingUpdatedEvent {
+    private Long roomId;
+    private Long matchId;
+    private String matchName;
+    private String difficulty;
+    private Integer maxUserCount;
+    private LocalDateTime startTime;
+
+    static public RoomSettingUpdatedEvent from (MatchSettingChangedEvent event){
+        return RoomSettingUpdatedEvent.builder()
+                .roomId(event.getRoomId())
+                .matchId(event.getMatchId())
+                .matchName(event.getMatchName())
+                .difficulty(event.getDifficulty())
+                .maxUserCount(event.getMaxUserCount())
+                .startTime(event.getStartTime())
+                .build();
+    }
+}
