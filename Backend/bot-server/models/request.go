09a2ad46b9c44e3ff42ae2ea5540218e90a5e55d
0224LJH <kavae550@gmail.com>
2025-11-07 12:54:41 +0900

    chore: json 매핑 이름 camelCase로 변경 [S13P31A209-145]

--- Backend/bot-server/models/request.go
+++ Backend/bot-server/models/request.go
@@ -4,7 +4,7 @@ import "time"
 
 // MatchSettingRequest 매치 시작 요청
 type MatchSettingRequest struct {
-	BotCount   int        `json:"bot_count" binding:"required,min=1,max=50000"`         // 봇 개수
-	StartTime  time.Time  `json:"start_time" binding:"required"`                        // 티케팅 시작 시간
+	BotCount   int        `json:"botCount" binding:"required,min=1,max=50000"`          // 봇 개수
+	StartTime  time.Time  `json:"startTime" binding:"required"`                         // 티케팅 시작 시간
 	Difficulty Difficulty `json:"difficulty" binding:"required,oneof=EASY MEDIUM HARD"` // 난이도
 }
