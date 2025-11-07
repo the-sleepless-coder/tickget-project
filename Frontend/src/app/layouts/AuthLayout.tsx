0655415ad59ae07daba685d0e7914f2a249d2564
임유나 <libbyyouna@gmail.com>
2025-11-07 10:12:57 +0900

    최상단 스크롤 적용

--- Frontend/src/app/layouts/AuthLayout.tsx
+++ Frontend/src/app/layouts/AuthLayout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import ScrollToTop from "../routes/ScrollToTop";
 
 export default function AuthLayout() {
   return (
     <div>
+      <ScrollToTop />
       <Outlet />
     </div>
   );
