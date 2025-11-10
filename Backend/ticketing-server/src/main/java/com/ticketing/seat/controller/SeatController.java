1a428795f6feda57f4ccf72af78768e607ac8999
hwi-lee <riteru66@never.com>
2025-11-10 17:57:27 +0900

    feat: RequestMapping url 수정

--- Backend/ticketing-server/src/main/java/com/ticketing/seat/controller/SeatController.java
+++ Backend/ticketing-server/src/main/java/com/ticketing/seat/controller/SeatController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/ticketing")
+@RequestMapping("/ticketing")
 public class SeatController {
 
     private final SeatReservationService seatReservationService;
