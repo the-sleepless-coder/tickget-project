8fb798a5eeab2fd64fb0dcc4400ee5e2b1583c00
DESKTOP-HA97GMI\SSAFY <h2sorginal@gmail.com>
2025-11-13 02:42:23 +0900

    fix: search-server 최초 배포

--- Backend/search-server/src/main/java/com/tickget/searchserver/SearchServerApplication.java
+++ Backend/search-server/src/main/java/com/tickget/searchserver/SearchServerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SearchServerApplication {
+    //search 서버 최초 배포용 주석
 
     public static void main(String[] args) {
         SpringApplication.run(SearchServerApplication.class, args);
