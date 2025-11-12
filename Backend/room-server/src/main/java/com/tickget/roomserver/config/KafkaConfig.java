9bd7bef4f6d9206100c9af15d66440276bbdd783
0224LJH <kavae550@gmail.com>
2025-11-12 10:01:49 +0900

    fix: kafkaConfig에 bean 추가

--- Backend/room-server/src/main/java/com/tickget/roomserver/config/KafkaConfig.java
+++ Backend/room-server/src/main/java/com/tickget/roomserver/config/KafkaConfig.java
@@ -5,18 +5,25 @@ import com.tickget.roomserver.event.RoomPlayingEndedEvent;
 import com.tickget.roomserver.event.RoomPlayingStartedEvent;
 import com.tickget.roomserver.event.RoomSettingUpdatedEvent;
 import com.tickget.roomserver.event.SessionCloseEvent;
+import com.tickget.roomserver.event.UserDequeuedEvent;
 import com.tickget.roomserver.event.UserJoinedRoomEvent;
 import com.tickget.roomserver.event.UserLeftRoomEvent;
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 @Configuration
@@ -70,5 +77,27 @@ public class KafkaConfig {
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         props.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, true);
         return props;
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, UserDequeuedEvent>
+    noTypeHeadersKafkaListenerContainerFactory() {
+
+        ConcurrentKafkaListenerContainerFactory<String, UserDequeuedEvent> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "room-server-" + System.getenv().getOrDefault("POD_NAME", "local"));
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
+        props.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, false);
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.tickget.roomserver.event");
+        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, UserDequeuedEvent.class);
+
+        factory.setConsumerFactory(new DefaultKafkaConsumerFactory<>(props));
+
+        return factory;
     }
 }
