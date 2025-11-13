cb83b038bd50d03a75a938c5b4d389a0ec4391b3
0224LJH <kavae550@gmail.com>
2025-11-13 11:31:22 +0900

    feat: 이미지 업로드 관련 예외 처리 구현

---
+++ Backend/room-server/src/main/java/com/tickget/roomserver/exception/FileUploadException.java
@@ -0,0 +1,12 @@
+package com.tickget.roomserver.exception;
+
+public class FileUploadException extends RuntimeException {
+
+    public FileUploadException(String message) {
+        super(message);
+    }
+
+    public FileUploadException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
