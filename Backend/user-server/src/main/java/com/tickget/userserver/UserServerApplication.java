2f568c1ae91dc741a1cd5be732aadce31bd0813d
DESKTOP-HA97GMI\SSAFY <h2sorginal@gmail.com>
2025-11-13 15:12:55 +0900

    fix:CI용 주석 추가

--- Backend/user-server/src/main/java/com/tickget/userserver/UserServerApplication.java
+++ Backend/user-server/src/main/java/com/tickget/userserver/UserServerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserServerApplication {
+    //CI용 주석 추가
 
     public static void main(String[] args) {
         SpringApplication.run(UserServerApplication.class, args);
