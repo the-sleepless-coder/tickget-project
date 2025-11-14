0201faf65216b3a34e4ca79d994fbcb91e578604
0224LJH <kavae550@gmail.com>
2025-11-14 20:34:19 +0900

    feat: 최소 봇 요청 수 변경

--- Backend/bot-server/models/request.go
+++ Backend/bot-server/models/request.go
@@ -2,7 +2,7 @@ package models
 
 // MatchSettingRequest 매치 시작 요청
 type MatchSettingRequest struct {
-	BotCount   int           `json:"botCount" binding:"required,min=1,max=50000"`          // 봇 개수
+	BotCount   int           `json:"botCount" binding:"required,min=0,max=50000"`          // 봇 개수
 	StartTime  LocalDateTime `json:"startTime"`                                            // 티케팅 시작 시간
 	Difficulty Difficulty    `json:"difficulty" binding:"required,oneof=EASY MEDIUM HARD"` // 난이도
 	HallID     int64         `json:"hallId" binding:"required"`                            // 공연장 ID
