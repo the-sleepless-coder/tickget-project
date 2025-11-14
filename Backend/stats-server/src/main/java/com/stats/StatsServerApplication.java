7fbe75fcfe691c73229e3c54ee7be047f1ea8036
DESKTOP-HA97GMI\SSAFY <h2sorginal@gmail.com>
2025-11-14 13:44:28 +0900

    fix: CI 주석 추가

--- Backend/stats-server/src/main/java/com/stats/StatsServerApplication.java
+++ Backend/stats-server/src/main/java/com/stats/StatsServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StatsServerApplication {
-    //CI용 주석
+    //CI용 주석222
     public static void main(String[] args) {
         SpringApplication.run(StatsServerApplication.class, args);
     }
