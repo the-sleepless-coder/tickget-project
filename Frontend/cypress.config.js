27aedd6b92f83951eb2baad85f1933063afc2084
임유나 <libbyyouna@gmail.com>
2025-11-16 15:45:10 +0900

    hotfix: 최신 빌드 배포

---
+++ Frontend/cypress.config.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var cypress_1 = require("cypress");
+exports.default = (0, cypress_1.defineConfig)({
+    e2e: {
+        baseUrl: "http://localhost:5173",
+        viewportWidth: 1280,
+        viewportHeight: 720,
+        video: true,
+        screenshotOnRunFailure: true,
+        setupNodeEvents: function (on, config) {
+            // implement node event listeners here
+        },
+        supportFile: "cypress/support/e2e.ts",
+        specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+        defaultCommandTimeout: 10000,
+        requestTimeout: 10000,
+        responseTimeout: 10000,
+    },
+});
---
+++ Frontend/cypress/support/commands.js
@@ -0,0 +1,3 @@
+"use strict";
+/// <reference types="cypress" />
+Object.defineProperty(exports, "__esModule", { value: true });
